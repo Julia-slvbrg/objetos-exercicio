db89ad2a591d4cf5affd504e973469c77c5c2f3f
Julia-slvbrg <julia.slvb@gmail.com>
2022-11-08 19:38:54 -0300

    Exercício 2 resolvido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,24 @@ const filme = {
 };
 
 // respostas aqui ⬇️
+
+//a) Nome do primeiro ator/atriz
+
+console.log(filme.elenco[0])
+
+//b) Nome do último ator/atriz
+
+console.log(filme.elenco[3])
+
+//c) Todas as transmissões de hoje
+
+console.log(filme.transmissoesHoje)
+
+//d) Horário de transmissão do filme no Canal Brasil
+
+//Para mostrar todo o objeto, canal mais horário
+console.log(filme.transmissoesHoje[1])
+
+// Para mostrar só o horário
+console.log(filme.transmissoesHoje[1].horario)
+
